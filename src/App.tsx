9c497aa85c2fcade2ed95a8d5b6af362a6bd073d
thecuongk5hp <147390984+thecuongk5hp@users.noreply.github.com>
2024-05-13 07:57:17 +0700

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Exercise01 from './components/Exercise01'
+import B1 from "./components/B1"
+
+export default function App() {
+  return (
+    <div>
+
+      <Exercise01/>
+      <BT1></BT1>
+      <BT2></BT2>
+    </div>
+  )
+}
